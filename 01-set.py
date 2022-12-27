d7e7b9287bba32356fc14b4d73cc8f9349431f5d
emivnajera <emivelasquez2000@hotmail.com>
2022-12-27 11:21:39 -0600

    added set file.

---
+++ 01-set.py
@@ -0,0 +1,15 @@
+set_countries = {'gt','mex','col'}
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = {1,2,2,3,4,5}
+print(set_numbers)
+
+set_types = {1,'hola', False, 12.12}
+print(set_types)
+
+set_from_string  = set('hola')
+print(set_from_string)
+
+set_from_tuples = set(('abc', 'cbv', 'as','abc'))
+print(set_from_tuples)
