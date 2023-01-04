334037e6c18aa345e548c1be531ff6a6cb88b4b8
emivnajera <emivelasquez2000@hotmail.com>
2023-01-04 14:09:05 -0600

    added return functions.

--- 06-functions.py
+++ 06-functions.py
@@ -13,3 +13,14 @@ def suma2(min, max):
     return sum
 
 print(suma2(1,10))
+
+## Parámetros por defecto y múltiples returns
+
+def find_volume(length = 1, width =1, depth =1):
+    return length * width * depth, width, 'hola'
+
+result, width, text = find_volume(width=10, length=5, depth=2)
+
+print(result)
+print(width)
+print(text)
